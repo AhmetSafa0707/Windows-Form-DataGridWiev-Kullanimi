400733f1ef9d131fc98bfc7d9c090bcdb7735adb
AhmetSafa0707 <119118562+AhmetSafa0707@users.noreply.github.com>
2023-06-24 16:12:56 +0300

    Data Grid Wiev Kullanımı
    ID isim ve soyisim bilgisi olan kullanıcı listesi oluşturmak için DataGriidWiev kullandım.Listeyi; Ekle,Sil ve Güncelle butonları ile düzenleyebiliriz.

---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows_Form_DataGridView_Kullanımı
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void groupBox1_Enter(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btn_ekle_Click(object sender, EventArgs e)
+		{
+			dataGridView1.Rows.Add(textBox1.Text,textBox2.Text,textBox3.Text);
+			textBox1.Text = string.Empty;
+			textBox2.Text = string.Empty;	
+			textBox3.Text = string.Empty;
+		}
+
+		private void btn_sil_Click(object sender, EventArgs e)
+		{
+			dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+		}
+
+		private void btn_guncelle_Click(object sender, EventArgs e)
+		{
+			dataGridView1.Rows.Remove(dataGridView1.CurrentRow);
+			dataGridView1.Rows.Add(textBox1.Text, textBox2.Text, textBox3.Text);
+
+		}
+	}
+}
